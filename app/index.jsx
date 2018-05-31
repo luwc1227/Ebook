d457cd09656c73283300699f327b115230fb3ee1
luwc1227 <1909765815@qq.com>
2018-05-31 20:18:24 +0800

    new project

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from 'react-dom'
+import './static/css/common.less'
+
+// 创建 Redux 的 store 对象
+class Hello extends React.Component{
+    render(){
+        return(
+            <div>hello world</div>
+        )
+    }
+}
+render(
+    <Hello/>,
+    document.getElementById('root')
+)
